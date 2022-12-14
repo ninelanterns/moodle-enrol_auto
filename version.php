aac2985bf1bb020fe70dfb574c1ca7a1ebb8af2f
kausweb <kaushtuvgurung@gmail.com>
2022-12-14 17:07:38 +1100

    Gitlab migration composer file update and version file update

--- version.php
+++ version.php
@@ -28,5 +28,4 @@ $plugin->version   = 2018032501;        // The current plugin version (Date: YYY
 $plugin->requires  = 2016120500;        // Requires this Moodle version
 $plugin->component = 'enrol_auto';      // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = 'melodie-r1';
-
+$plugin->release = \local_core\ComposerPluginInfo::getInstance()->getInstalledPluginVersion('pluginname');
